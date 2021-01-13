378a155017913faebdf95ef67371648e36a4f52d
Marcus van Winden <marcus.van.winden@icloud.com>
2021-01-13 14:45:54 +0100

    Add PaletteList, Navbar, and Palette components

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find((palette) => palette.id === id);
+  }
+
   render() {
-    console.log(generatePalette(seedColors[4]));
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <PaletteList palettes={seedColors} />}
+        />
+        <Route
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
